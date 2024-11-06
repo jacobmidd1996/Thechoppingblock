1c5e203e8900f1f9af27f9f0198c41d787ebccbd
jacobmidd1996 <debacled1996@gmail.com>
2024-11-06 14:35:13 -0500

    adjusted json, added installs for nav

--- app.tsx
+++ app.tsx
@@ -1,10 +1,9 @@
-// App.js or App.tsx
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import ExploreRecipes from "./pages/ExploreRecipes";
-import Favorites from "./pages/Favorites";
-import Navbar from "./components/Navbar";
+import Home from "./home";
+import Explorerecipes from "./Explorerecipes";
+import Favorites from "./Favorites";
+import Navbar from "./Nav";
 
 function App() {
   return (
@@ -12,7 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/explore-recipes" element={<ExploreRecipes />} />
+        <Route path="/explore-recipes" element={<Explorerecipes />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
