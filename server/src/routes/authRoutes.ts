a26320330e7a8f95fd8027cc3dff06f340a6d7f4
jacobmidd1996 <debacled1996@gmail.com>
2024-11-08 11:21:54 -0500

    you alreaedy know

--- server/src/routes/authRoutes.ts
+++ server/src/routes/authRoutes.ts
@@ -1,5 +1,5 @@
 import { Router, type Request, type Response } from "express";
-import { User } from "../models/user.js";
+import { User } from "../models/Users.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
