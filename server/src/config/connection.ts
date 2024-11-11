419f1018b670cc7fd4242c216c5282ae4ddbe1f4
jacobmidd1996 <debacled1996@gmail.com>
2024-11-11 13:10:37 -0500

    sequilize

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,23 +1,38 @@
-//Using activity 25 as boilerplate for connection
+let sequelize;
 
-import dotenv from 'dotenv';
-dotenv.config();
+if (process.env.DB_URL) {
+  sequelize = new sequelize(process.env.DB_URL);
+} else {
+  sequelize = new sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PW,
+    {
+      host: "localhost",
+      dialect: "postgres",
+    }
+  );
+}
+// //Using activity 25 as boilerplate for connection
 
-import { Sequelize } from 'sequelize';
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(
-      process.env.DB_NAME || '',
-      process.env.DB_USER || '',
-      process.env.DB_PASSWORD,
-      {
-        host: 'localhost',
-        dialect: 'postgres',
-        dialectOptions: {
-          decimalNumbers: true,
-        },
-      }
-    );
+// import { Sequelize } from 'sequelize';
 
-export default sequelize;
+// const sequelize = process.env.DB_URL
+//   ? new Sequelize(process.env.DB_URL)
+//   : new Sequelize(
+//       process.env.DB_NAME || '',
+//       process.env.DB_USER || '',
+//       process.env.DB_PASSWORD,
+//       {
+//         host: 'localhost',
+//         dialect: 'postgres',
+//         dialectOptions: {
+//           decimalNumbers: true,
+//         },
+//       }
+//     );
+
+// export default sequelize;
