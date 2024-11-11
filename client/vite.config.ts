8c573837dcbf7b4f8b95725a996968cff5dcec19
Katrina Moody <kmfoster212@gmail.com>
2024-11-10 20:00:17 -0500

    Updated packages

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -6,16 +6,17 @@ export default defineConfig({
     port: 3000,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      "/api": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
-      '/auth': {
-        target: 'http://localhost:3001',
+      "/auth": {
+        target: "http://localhost:3001",
         changeOrigin: true,
         secure: false,
       },
     },
   },
+  envDir: "../server",
 });
